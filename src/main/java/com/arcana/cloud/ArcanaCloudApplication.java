fa7f2733ae8732cb9e585029f18f8fce547938f6
jrjohn <jr.johnchang@gmail.com>
2026-01-05 18:20:51 +0800

    fix eclipse open project error

--- src/main/java/com/arcana/cloud/ArcanaCloudApplication.java
+++ src/main/java/com/arcana/cloud/ArcanaCloudApplication.java
@@ -2,10 +2,11 @@ package com.arcana.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.grpc.server.autoconfigure.health.GrpcServerHealthAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = GrpcServerHealthAutoConfiguration.class)
 @EnableCaching
 @EnableJpaAuditing
 public class ArcanaCloudApplication {
